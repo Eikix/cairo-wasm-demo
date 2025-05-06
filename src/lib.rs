6871fa513607bc63b19a5672419036dbefe8d896
Elias Tazartes <elias.tazartes@gmail.com>
2025-05-06 15:32:42 +0200

    revert to old stwo commit

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use cairo_vm::{
 use wasm_bindgen::prelude::*;
 
 use cairo_air::{verifier::verify_cairo, CairoProof, PreProcessedTraceVariant};
-use stwo_cairo_adapter::{adapter::adapt_finished_runner, ProverInput};
+use stwo_cairo_adapter::{plain::adapt_finished_runner, ProverInput};
 use stwo_cairo_prover::{
     prover::prove_cairo,
     stwo_prover::core::vcs::blake2_merkle::{Blake2sMerkleChannel, Blake2sMerkleHasher},
@@ -44,7 +44,7 @@ pub fn run_cairo_program() -> Result<CairoRunner, CairoRunError> {
         entrypoint: "main",
         trace_enabled: true,
         relocate_mem: true,
-        layout: LayoutName::all_cairo_stwo,
+        layout: LayoutName::all_cairo,
         proof_mode: true,
         secure_run: Some(true),
         allow_missing_builtins: Some(false),
